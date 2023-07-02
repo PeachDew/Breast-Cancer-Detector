0e7a561a43116ac55985fd8481957fdb72ac13c2
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-02 17:56:41 +0800

    Debuggin webapp

--- streamlit/Tumor_Classification.py
+++ streamlit/Tumor_Classification.py
@@ -16,14 +16,13 @@ st.set_page_config(page_title="Tumor Classification")
 
 st.sidebar.header("Predicting Gender from Twitter Profile.")
 st.markdown("""
-# Gender/Brand Prediction 📱
-This app utilizes machine learning to make predictions based on a Twitter user profile. Simply provide us with some information about a Twitter user, and we'll generate a prediction for you!
+# Malignant/Benign Tumore Classification using Random Forest
 """
 )
 
-# Load the models
-#with open("./Gender_Model_Save/best_clf.pkl", "rb") as file:
-#    name_model = pickle.load(file)
+#Load the models
+with open("./rf_model.pkl", "rb") as file:
+    rf_model = pickle.load(file)
        
     
 col1, col2 = st.columns(2)
@@ -37,9 +36,11 @@ with col1:
     perim = st.slider('Choose perimeter', min_value=0.0,
                     max_value=50.0,
                     value=25.0)
+    # Shape
     smooth = st.slider('Choose smoothness', min_value=0.0,
                     max_value=50.0,
                     value=25.0)
+    # Roughness
     text = st.slider('Choose texture', min_value=0.0,
                     max_value=50.0,
                     value=25.0)
@@ -49,7 +50,18 @@ with col2:
     pred_button = st.button('Generate Prediction')
     if pred_button:
         with st.spinner('Wait for it...'):
-            st.balloons()
-            st.markdown("Yay!")
+            data = [[area, text, perim, area, smooth]]
+            prediction = rf_model.predict(data)
+            probabilities = rf_model.predict_proba(data)
+            
+            # Display the prediction
+            if prediction == 0:
+                st.write("The tumor is predicted to be benign.")
+            else:
+                st.write("The tumor is predicted to be malignant.")
+            st.write("Benign Probability:", probabilities[0][0])
+            st.write("Malignant Probability:", probabilities[0][1])
+            
+            
             
 
